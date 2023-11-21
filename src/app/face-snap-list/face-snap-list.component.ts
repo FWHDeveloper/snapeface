428b1772c103ef22e0cde65f91b470802dc25833
FWHDeveloper <contact.fbm3@gmail.com>
2023-11-21 21:04:59 +0100

    Partagez des données avec les Services

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
-
+import { FaceSnap } from 'src/models/face-snap.model';
+import { FaceSnapsService } from '../services/face-snape.service'
 @Component({
   selector: 'app-face-snap-list',
   templateUrl: './face-snap-list.component.html',
-  styleUrls: ['./face-snap-list.component.scss']
+  styleUrls: ['./face-snap-list.component.scss'],
 })
 export class FaceSnapListComponent {
-
+  faceSnaps!: FaceSnap[];
+  constructor(private faceSnapService:FaceSnapsService){}
+  ngOnInit() {
+    this.faceSnaps = this.faceSnapService.faceSnaps;
+  }
 }
